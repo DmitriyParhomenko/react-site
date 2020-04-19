78ec895050031ca3a117d2f16aa73a00e225c9a6
Dmitriy Parhomenko <dmitriyparhomenko140@gmail.com>
2020-04-19 16:25:11 +0300

    Updates

---
+++ precache-manifest.ae7819900237d8545c0e9573543721ca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9bb96f74eeb10bd59f918119c2be2d0",
+    "url": "/react-site/index.html"
+  },
+  {
+    "revision": "ea3207b36f5284151d97",
+    "url": "/react-site/static/css/main.06a6eca1.chunk.css"
+  },
+  {
+    "revision": "bf548a93f58f49de994c",
+    "url": "/react-site/static/js/2.5dd03ae8.chunk.js"
+  },
+  {
+    "revision": "b17efaeb5447a326300bf38cc78394ad",
+    "url": "/react-site/static/js/2.5dd03ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea3207b36f5284151d97",
+    "url": "/react-site/static/js/main.6a5fa0d8.chunk.js"
+  },
+  {
+    "revision": "4052abc3e9098cda5cc3",
+    "url": "/react-site/static/js/runtime-main.14f108d8.js"
+  },
+  {
+    "revision": "479c23b41aa026f18b332af30ba2f392",
+    "url": "/react-site/static/media/feat1.479c23b4.jpg"
+  },
+  {
+    "revision": "170c7e3a35d57efe4af40f074029550e",
+    "url": "/react-site/static/media/feat2.170c7e3a.jpg"
+  },
+  {
+    "revision": "1fc6f6bb62a51e027bb28a547af30719",
+    "url": "/react-site/static/media/feat3.1fc6f6bb.jpg"
+  },
+  {
+    "revision": "87b93cee43d0d1f752f34236612f2f6c",
+    "url": "/react-site/static/media/slide1.87b93cee.jpg"
+  },
+  {
+    "revision": "3257a8736ca2d05a08dd2c00dc04db0b",
+    "url": "/react-site/static/media/slide2.3257a873.jpg"
+  },
+  {
+    "revision": "6862311308483b15392b7301a9bcc53b",
+    "url": "/react-site/static/media/slide3.68623113.jpg"
+  }
+]);
